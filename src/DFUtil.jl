b8e4fde41a7b6fd6fdbc7cff2e44cf10c98df3aa
matthew.heath <matthew.heath@wrenkitchens.com>
2021-11-09 17:10:55 +0000

    multi depth keys in json export

--- src/DFUtil.jl
+++ src/DFUtil.jl
@@ -5,9 +5,20 @@ using Dates
 
 export sum_columns, group_data_into_periods, match_row, to_json, to_json_var
 
-function sum_columns(df, group_by::Vector{String}) 
-	buffer = combine(groupby(df, group_by),  [ c => c->sum(skipmissing(c)) for c in filter(n->!(n in group_by), names(df)) ])
-	rename(buffer, map(c-> c=>replace(c, "_function"=>"s"), names(buffer)))
+"""
+	sum_columns(df, group_by::Vector{String}=Vector{String}())
+
+	sum the columns of the dataframe, optionally grouping by group_by
+
+# Arguments
+
+- `df` DataFrame to sum
+- `group_by` Vector of String names of the columns to group by
+- `replace_with` by default DataFrames takes the column names and appends _function. Instead use this string.
+"""
+function sum_columns(df; group_by::Vector{String}=Vector{String}(), replace_with="s") 
+	buffer = combine(group_by == [] ? df : groupby(df, group_by),  [ c => c->sum(skipmissing(c)) for c in filter(n->!(n in group_by), names(df)) ])
+	rename(buffer, map(c-> c=>replace(c, "_function"=>replace_with), names(buffer)))
 end	
 
 function group_data_into_periods(df, date_column, period; andgrpby = Vector{String}())
@@ -21,7 +32,7 @@ function group_data_into_periods(df, date_column, period; andgrpby = Vector{Stri
 	
 	df[!, date_column * "_period"] = map(dt->period_fns[period](dt), df[!, date_column])  
 
-	sum_columns(select(df, Not(date_column)), vcat(andgrpby, [date_column * "_period"]))
+	sum_columns(select(df, Not(date_column)), group_by=vcat(andgrpby, [date_column * "_period"]))
 	
 end
 
@@ -32,27 +43,58 @@ end
 jesc(v) = replace(string(v), "'"=>"\\'")
 kesc(k) = replace(jesc(k), " "=>"")
 
-print_term(io, row, key; prefix="") = print(io, prefix, "\"$(kesc(key))\" : ", "\"$(jesc(row[key]))\""); 
+print_term(io, row, key, prefix="") = print(io, prefix, "\"$(kesc(key))\" : ", "\"$(jesc(row[key]))\""); 
 
-function print_data_row(io, row, pkey, exports; prefix="")
+function print_terms(io, row)
+	print_term(io, row, names(row)[1], "")
+	broadcast(term -> print_term(io, row, term, ", "), names(row)[2:end]);
+end
+
+function print_data_row(io, row, pkey, prefix="")
 	print(io, prefix, "\"$(kesc(row[pkey]))\" : {")
-	print_term(io, row, exports[1])
-	broadcast(term -> print_term(io, row, term; prefix=", "), exports[2:end]);
+	print_terms(io, row)
 	print(io, " } ");
 end
 
-function to_json(io::IO, data::DataFrame, pkey::AbstractString)
-	exports = filter(k->k!=pkey, names(data))
+function to_jsonX(io::IO, data::DataFrame, pkey::AbstractString)
 	print(io, "{ ")
-	print_data_row(io, eachrow(data)[1], pkey, exports)
-	broadcast(row->print_data_row(io, row, pkey, exports; prefix=", "), eachrow(data)[2:end])
+	print_data_row(io, eachrow(data)[1], pkey)
+	broadcast(row->print_data_row(io, row, pkey; prefix=", "), eachrow(data)[2:end])
 	print(io, " }");
 end
 
-function to_json_var(io::IO, data::DataFrame, pkey::AbstractString, var="object")
-	print("var $var=JSON.parse('")
-	to_json(io, data, pkey)
+function to_json_var(io::IO, data::DataFrame, pkeys, var="object")
+	print(io, "var $var=JSON.parse('")
+	to_json(io, data, isa(pkey, Array) ? pkey : [pkey])
 	println(io, "');");
+end
+
+function to_json(io::IO, data::DataFrame, keys)
+	print(io, "{ ")
+	if isa(keys, Array)
+		to_json(io, groupby(data, keys[1]), keys, depth=1)
+	else
+		to_json(io, groupby(data, keys), [keys], depth=1)
+	end
+	print(io, " }")
+end
+
+function to_json(io::IO, data, keys; depth=1)
+	prefix = ""
+	if depth == length(keys)
+		for grp in data
+			print_data_row(io, eachrow(grp)[1], keys[depth], prefix)
+			prefix=", "
+		end
+	else
+		for grp in data 
+			k = kesc(grp[!, keys[depth]][1])
+			print(io, prefix, "\"$k\" : {")
+			to_json(io, groupby(grp, keys[depth+1]), keys, depth=depth+1)
+			print(io, " }");
+			prefix = ", "
+		end
+	end
 end
 
 
